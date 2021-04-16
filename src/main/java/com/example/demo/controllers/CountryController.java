8e125d93ab1cb4b3431fd9618e86a768b3604e8d
codenet_anhpq <anhpq@codenet.jp>
2021-04-16 16:39:58 +0700

    add countryprovince

--- src/main/java/com/example/demo/controllers/CountryController.java
+++ src/main/java/com/example/demo/controllers/CountryController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.CountryProvince;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -30,5 +31,11 @@ public class CountryController {
 	public List<Country> getAllCountry () {
 		String sql = "SELECT * FROM country";
 		return jdbcTemplate.query(sql, new HashMap<>(), new BeanPropertyRowMapper<Country>(Country.class));
+	}
+
+	@GetMapping("country/province")
+	public List<CountryProvince> getAllCountryWithProvince() {
+		String sql = "SELECT c.id id, c.name name, p.name FROM country as c LEFT JOIN province as p ON p.country = c.id";
+		return jdbcTemplate.query(sql, new HashMap<>(), new BeanPropertyRowMapper<CountryProvince>(CountryProvince.class));
 	}
 }
---
+++ src/main/java/com/example/demo/models/CountryProvince.java
@@ -0,0 +1,33 @@
+package com.example.demo.models;
+
+import java.util.List;
+
+public class CountryProvince {
+    private int id;
+    private int name;
+    private List<Province> provinceList;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public List<Province> getProvinceList() {
+        return provinceList;
+    }
+
+    public void setProvinceList(List<Province> provinceList) {
+        this.provinceList = provinceList;
+    }
+}
